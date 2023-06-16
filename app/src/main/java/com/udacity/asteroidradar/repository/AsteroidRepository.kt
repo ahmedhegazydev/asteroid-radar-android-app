5c5eca4bcd7b933efafd78e36cc9e248240729b7
Ahmed Hegazy <ahmed.hegazy2@vodafone.com>
2023-06-16 16:48:00 +0200

    Asteroid Radar App

---
+++ app/src/main/java/com/udacity/asteroidradar/repository/AsteroidRepository.kt
@@ -0,0 +1,42 @@
+package com.udacity.asteroidradar.repository
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.map
+import com.udacity.asteroidradar.api.*
+import com.udacity.asteroidradar.database.AsteroidsDatabase
+import com.udacity.asteroidradar.database.asDomainModel
+import com.udacity.asteroidradar.domain.Asteroid
+import com.udacity.asteroidradar.util.parseAsteroidsJsonResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.lang.Exception
+
+class AsteroidRepository(private val database: AsteroidsDatabase) {
+
+    val asteroids: LiveData<List<Asteroid>> = database.asteroidDao.getAsteroid().map {
+        it.asDomainModel()
+    }
+
+    fun getWeekAsteroids(startDate: String, endDate: String) = database.asteroidDao.getWeekAsteroids(startDate, endDate)
+
+    fun getTodayAsteroids(todayDate: String) = database.asteroidDao.getTodayAsteroids(todayDate)
+
+    fun deletePreviousDayAsteroid(date: String) = database.asteroidDao.deletePreviousDayAsteroids(date)
+
+    suspend fun refreshAsteroid(startDate: String, endDate: String, apiKey: String) {
+        withContext(Dispatchers.IO) {
+            try {
+                val resultList = AsteroidApi.retrofitService.getAsteroidProperties(startDate, endDate, apiKey)
+                val asteroidList = parseAsteroidsJsonResult(JSONObject(resultList))
+                database.asteroidDao.insertAll(*asteroidList.asDatabaseModel())
+            } catch (e: Exception) {
+                Log.i("Failure", e.message!!)
+
+            }
+        }
+    }
+
+    suspend fun getPictureOfTheDay(apiKey: String) = PictureOfDayApi.retrofitService.getImageOfTheDay(apiKey)
+}
---
+++ app/src/main/java/com/udacity/asteroidradar/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.udacity.asteroidradar.util
+
+object Constants {
+    const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
+    const val DEFAULT_END_DATE_DAYS = 5
+    const val BASE_URL = "https://api.nasa.gov/"
+    const val API_KEY = "Ch7aaymiIT8ne9mICCPIxjqW2ffTK3tS7x0xHgir"
+
+}
